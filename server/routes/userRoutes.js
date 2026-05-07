6bb01a57fb5b2556e054328e869ebf092388ea5f
Sai Vivek <saivivekbunny69@gmail.com>
2026-05-07 10:26:34 +0530

    project completed

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from "express"
+import{ registerUser, getUserById, loginUser, getUserResumes } from "../controllers/userController.js"
+import protect from "../middlewares/authMiddleware.js"
+const userRouter=express.Router()
+
+userRouter.post("/register",registerUser)
+userRouter.post("/login",loginUser)
+userRouter.get("/data",protect,getUserById)
+userRouter.get("/resumes",protect,getUserResumes)
+export default userRouter
