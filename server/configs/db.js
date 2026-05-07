6aa30e48b26a103841b53c9d668758103eef09b2
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2026-05-07 11:41:48 +0530

    Refactor connectDB function for improved clarity

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,24 +1,23 @@
 import mongoose from "mongoose"
 
-const connectDB=async()=>{
-    try{
-        mongoose.connection.on("connected",()=>{
-            console.log("Database connected successfully")
-        })
-        let mongodbURI=process.env.MONGODB_URI;
-        const projectName="resume-builder";
-        if(!mongodbURI){
-            throw new Error("MONGODB_URI environment variable not set")
-        }
-        if(mongodbURI.endsWith("/")){
-            mongodbURI=mongodbURI.slice(0,-1)
-        }
-        await mongoose.connect(`${mongodbURI}/${projectName}`)
+const connectDB = async () => {
+  try {
 
-    }
-    catch(error){
-        console.error("Error connecting to MongoDB:",error)
+    mongoose.connection.on("connected", () => {
+      console.log("Database connected successfully")
+    })
 
+    const mongodbURI = process.env.MONGODB_URI
+
+    if (!mongodbURI) {
+      throw new Error("MONGODB_URI environment variable not set")
     }
+
+    await mongoose.connect(mongodbURI)
+
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error)
+  }
 }
+
 export default connectDB
